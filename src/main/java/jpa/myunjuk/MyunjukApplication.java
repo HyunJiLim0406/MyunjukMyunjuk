0e7c6392d2df7b1b1f29f7ab228d028ceda3d6ba
iw0406 <hyunjilim98@gmail.com>
2021-07-15 14:24:39 +0900

    프로젝트 생성

---
+++ src/main/java/jpa/myunjuk/MyunjukApplication.java
@@ -0,0 +1,13 @@
+package jpa.myunjuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyunjukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyunjukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/myunjuk/MyunjukApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.myunjuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyunjukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
