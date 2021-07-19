17001ea0f32094cdea62f206f49539e20634e424
iw0406 <hyunjilim98@gmail.com>
2021-07-19 17:38:40 +0900

    User domain

---
+++ src/main/java/jpa/myunjuk/module/model/domain/User.java
@@ -0,0 +1,76 @@
+package jpa.myunjuk.module.model.domain;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @Column(name = "user_sn")
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    @NotNull
+    private String email;
+
+    @Column(length = 300)
+    @NotNull
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<UserCharacter> userCharacters = new ArrayList<>();
+
+    @Setter
+    private String refreshTokenValue;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
