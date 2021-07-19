b00f12a7936f70f1e96eaa9cbff681fc905e80e1
iw0406 <hyunjilim98@gmail.com>
2021-07-19 17:39:12 +0900

    Characters domain

---
+++ src/main/java/jpa/myunjuk/module/model/domain/Characters.java
@@ -0,0 +1,38 @@
+package jpa.myunjuk.module.model.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Characters {
+
+    @Id
+    @Column(name = "character_sn")
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String img;
+
+    private Integer height;
+
+    @NotNull
+    private LocalDate birthday;
+
+    @NotNull
+    private String shortDescription;
+
+    @Lob
+    @NotNull
+    private String longDescription;
+}
