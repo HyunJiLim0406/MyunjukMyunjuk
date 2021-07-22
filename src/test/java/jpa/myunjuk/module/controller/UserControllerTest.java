46688dd4b19d501256067c48251aad38f0bb2f90
iw0406 <hyunjilim98@gmail.com>
2021-07-22 15:13:09 +0900

    [Create] Add test for User api

--- src/test/java/jpa/myunjuk/module/controller/UserControllerTest.java
+++ src/test/java/jpa/myunjuk/module/controller/UserControllerTest.java
@@ -3,8 +3,6 @@ package jpa.myunjuk.module.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jpa.myunjuk.module.model.dto.user.UserSignInReqDto;
 import jpa.myunjuk.module.model.dto.user.UserSignUpReqDto;
-import jpa.myunjuk.module.repository.UserRepository;
-import jpa.myunjuk.module.service.UserService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- src/test/java/jpa/myunjuk/module/service/UserServiceTest.java
+++ src/test/java/jpa/myunjuk/module/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package jpa.myunjuk.module.service;
 
 import jpa.myunjuk.module.model.domain.User;
+import jpa.myunjuk.module.model.dto.user.UserSignUpReqDto;
 import jpa.myunjuk.module.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,14 +16,30 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class UserServiceTest {
 
-    @Autowired private UserRepository userRepository;
-    @Autowired private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName("User Sing-Up | Success")
+    void singUpSuccess() throws Exception {
+        UserSignUpReqDto userSignUpReqDto = UserSignUpReqDto.builder()
+                .email("new@new.com")
+                .password("1234")
+                .nickname("hello")
+                .build();
+        User user = userService.signUp(userSignUpReqDto);
+        assertEquals(user.getUserCharacters().size(), 1);
+        assertTrue(user.getUserCharacters().get(0).isRepresentation());
+    }
 
     @Test
     @DisplayName("User Sign-Out | Success")
     void signOutSuccess() throws Exception {
-        User user = userRepository.findById(1L).orElse(null);
+        User user = userRepository.findByEmail("test@test.com").orElse(null);
         assertNotNull(user);
+        assertNotNull(user.getRefreshTokenValue());
         userService.signOut(user);
         assertNull(user.getRefreshTokenValue());
     }
