18e1417ac5b1306828ff655cad88095776c9a891
iw0406 <hyunjilim98@gmail.com>
2021-07-22 15:10:57 +0900

    [Update] Add log

--- src/main/java/jpa/myunjuk/module/controller/UserController.java
+++ src/main/java/jpa/myunjuk/module/controller/UserController.java
@@ -6,6 +6,7 @@ import jpa.myunjuk.module.model.dto.user.UserSignUpReqDto;
 import jpa.myunjuk.module.model.dto.user.UserSignInReqDto;
 import jpa.myunjuk.module.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -22,11 +24,13 @@ public class UserController {
     /**
      * 회원가입
      * localhost:8080/sign-up
+     *
      * @param userSignUpReqDto
      * @return ResponseEntity
      */
     @PostMapping("/sign-up")
-    public ResponseEntity<?> join(@Valid @RequestBody UserSignUpReqDto userSignUpReqDto){
+    public ResponseEntity<?> join(@Valid @RequestBody UserSignUpReqDto userSignUpReqDto) {
+        log.info("[Request] sign-up");
         userService.signUp(userSignUpReqDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -34,33 +38,39 @@ public class UserController {
     /**
      * 로그인
      * localhost:8080/sign-in
+     *
      * @param userSignInReqDto
      * @return ResponseEntity
      */
     @PostMapping("/sign-in")
-    public ResponseEntity<?> signIn(@Valid @RequestBody UserSignInReqDto userSignInReqDto){
+    public ResponseEntity<?> signIn(@Valid @RequestBody UserSignInReqDto userSignInReqDto) {
+        log.info("[Request] sign-in " + userSignInReqDto.toString());
         return new ResponseEntity<>(userService.signIn(userSignInReqDto), HttpStatus.OK);
     }
 
     /**
      * 토큰 재발행
      * localhost:8080/refresh-tokens
+     *
      * @param jwtRefreshReqDto
      * @return ResponseEntity
      */
     @PostMapping("/refresh-tokens")
-    public ResponseEntity<?> refreshUserToken(@Valid @RequestBody JwtRefreshReqDto jwtRefreshReqDto){
+    public ResponseEntity<?> refreshUserToken(@Valid @RequestBody JwtRefreshReqDto jwtRefreshReqDto) {
+        log.info("[Request] refresh-tokens");
         return new ResponseEntity<>(userService.refreshUserTokens(jwtRefreshReqDto), HttpStatus.OK);
     }
 
     /**
      * 로그아웃
      * localhost:8080/sign-out
+     *
      * @param user
      * @return ResponseEntity
      */
     @PostMapping("/sign-out")
-    public ResponseEntity<?> signOut(@AuthenticationPrincipal User user){
+    public ResponseEntity<?> signOut(@AuthenticationPrincipal User user) {
+        log.info("[Request] sign-out");
         userService.signOut(user);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
